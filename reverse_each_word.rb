be04be7da44bc46f55e3c410637867ff8b624650
Lauren Yu <laurenkyu@gmail.com>
2020-01-23 17:38:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,14 @@
-def reverse_each_word(string)
-  separate_words = string.split
+# def reverse_each_word(string)
+#   separate_words = string.split
   
-  i = 0 
-  reversed_words = []
-  while i < separate_words.length 
-  reversed_words << separate_words[i].reverse
-  i += 1 
-  end
- reversed_sentence = reversed_words.join(" ")
- reversed_sentence
-end
+#   i = 0 
+#   reversed_words = []
+#   while i < separate_words.length 
+#   reversed_words << separate_words[i].reverse
+#   i += 1 
+#   end
+# reversed_sentence = reversed_words.join(" ")
+# reversed_sentence
+# end
 
-p reverse_each_word("Hi there!")
+# p reverse_each_word("Hi there!")
