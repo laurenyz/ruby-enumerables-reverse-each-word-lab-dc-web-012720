39aab7da842b9fa52af57d8ee1cda0059e73b7d7
Lauren Yu <laurenkyu@gmail.com>
2020-01-23 17:57:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -16,7 +16,9 @@
 def reverse_each_word(string)
   separate_words = string.split
   
-  reversed_words = separate_words.each do |word| p word.reverse
+  reversed_words = []
+  separate_words.each do |word| 
+    reversed_words << word.reverse
   end 
   reversed_words
 end 
